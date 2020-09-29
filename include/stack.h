b4a0553b09be7209ce18d1ed0daa63c1398b3876
Cillian O'Brien <cillianobrien01@gmail.com>
2020-09-29 01:13:54 +0100

    hidding stack struct from user

--- include/stack.h
+++ include/stack.h
@@ -4,17 +4,13 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-typedef struct stack {
-	int32_t* buffer;
-	uint32_t capacity;
-	uint32_t num_items;
-} stack_tt;
+typedef struct stack_s* Stack_T;
 
-stack_tt* create_stack(int32_t* buffer, uint32_t capacity);
-bool is_stack_full(stack_tt* stack);
-bool is_stack_empty(stack_tt* stack);
-void push_stack(stack_tt* stack, int32_t item);
-int32_t pop_stack(stack_tt* stack);
-int32_t peek_stack(stack_tt* stack);
+Stack_T create_stack(int32_t* buffer, uint32_t capacity);
+bool is_stack_full(Stack_T stack);
+bool is_stack_empty(Stack_T stack);
+void push_stack(Stack_T stack, int32_t item);
+int32_t pop_stack(Stack_T stack);
+int32_t peek_stack(Stack_T stack);
 
 #endif // __STACK__
--- source/stack.c
+++ source/stack.c
@@ -5,37 +5,43 @@
 
 #include "stack.h"
 
-stack_tt* create_stack(int32_t* buffer, uint32_t capacity) {
-	stack_tt* stack = (stack_tt*)malloc(sizeof(stack_tt));
+struct stack_s {
+	int32_t* buffer;
+	uint32_t capacity;
+	uint32_t num_items;
+};
+
+Stack_T create_stack(int32_t* buffer, uint32_t capacity) {
+	Stack_T stack = (Stack_T)malloc(sizeof(struct stack_s));
 	stack->buffer = buffer;
 	stack->capacity = capacity;
 	stack->num_items = 0;
 	return stack;
 }
 
-bool is_stack_full(stack_tt* stack) {
+bool is_stack_full(Stack_T stack) {
 	return stack->capacity == stack->num_items;
 }
 
-bool is_stack_empty(stack_tt* stack) {
+bool is_stack_empty(Stack_T stack) {
 	return stack->num_items == 0;
 }
 
-void push_stack(stack_tt* stack, int32_t item) {
+void push_stack(Stack_T stack, int32_t item) {
 	if(is_stack_full(stack)) {
 		return;
 	}
 	stack->buffer[stack->num_items++] = item;
 }
 
-int32_t pop_stack(stack_tt* stack) {
+int32_t pop_stack(Stack_T stack) {
 	if(is_stack_empty(stack)) {
 		return -1;
 	}
 	return stack->buffer[--stack->num_items];
 }
 
-int32_t peek_stack(stack_tt* stack) {
+int32_t peek_stack(Stack_T stack) {
 	if(is_stack_empty(stack)) {
 		return -1;
 	}
